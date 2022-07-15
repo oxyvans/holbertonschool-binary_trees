97528f0f08e7f0b4eace0d06af360aa609d47f56
oxyvans <benja.1999.g@hotmail.com>
2022-07-15 17:21:13 +0000

    add task 4

---
+++ 4-binary_tree_is_leaf.c
@@ -0,0 +1,16 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_delete - main.
+ * @node: tree
+ *
+ * Return: int 
+ **/
+
+int binary_tree_is_leaf(const binary_tree_t *node)
+{
+    if (node->left == NULL && node->right == NULL)
+        return (1);
+    else
+        return (0);
+}
