53ed2d6cefd51903ee210eeaf85927609de4e22b
oxyvans <benja.1999.g@hotmail.com>
2022-07-19 07:52:10 -0700

    add task 2

--- 2-binary_tree_insert_right.c
+++ 2-binary_tree_insert_right.c
@@ -4,37 +4,35 @@
  * binary_tree_insert_right - main.
  * @parent: nodo.
  * @value: int
- * 
  * Return: nodo
  **/
 
 binary_tree_t *binary_tree_insert_right(binary_tree_t *parent, int value)
 {
-    binary_tree_t *nodo, *aux;
+	binary_tree_t *nodo, *aux;
 
-    if (parent == NULL)
-        return (NULL);
+	if (parent == NULL)
+		return (NULL);
 
-    nodo = malloc(sizeof(binary_tree_t));
-    if (nodo == NULL)
-        return (NULL);
+	nodo = malloc(sizeof(binary_tree_t));
+	if (nodo == NULL)
+		return (NULL);
 
-    nodo->n = value;
-    nodo->left = nodo->right = NULL;
-    nodo->parent = parent;
+	nodo->n = value;
+	nodo->left = nodo->right = NULL;
+	nodo->parent = parent;
 
-    aux = parent->right;
+	aux = parent->right;
 
-    if (parent->right == NULL)
-    {
-        parent->right = nodo;
-    }
-    else
-    {
-        parent->right = nodo;
-        aux->parent = nodo;
-        nodo->right = aux;
-
-    }
-    return (nodo);
+	if (parent->right == NULL)
+	{
+		parent->right = nodo;
+	}
+	else
+	{
+		parent->right = nodo;
+		aux->parent = nodo;
+		nodo->right = aux;
+	}
+	return (nodo);
 }
