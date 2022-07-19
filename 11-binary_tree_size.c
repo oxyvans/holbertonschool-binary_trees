c77372dab39b0ef81856a92fe11639fd7126e7f9
oxyvans <benja.1999.g@hotmail.com>
2022-07-19 11:21:41 -0700

    add task 11

--- 11-binary_tree_size.c
+++ 11-binary_tree_size.c
@@ -1,12 +1,14 @@
 #include "binary_trees.h"
 
 /**
- * binary_tree_depth - main.
+ * binary_tree_size - main.
  * @tree: tree
  * Return: size
  **/
 
 size_t binary_tree_size(const binary_tree_t *tree)
 {
-
+	if (tree == NULL)
+		return (0);
+	return (binary_tree_size(tree->left) + binary_tree_size(tree->right) + 1);
 }
