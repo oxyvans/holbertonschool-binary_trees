d2678bed2da0fa4b61025b60eb276c8116d81fc2
oxyvans <benja.1999.g@hotmail.com>
2022-07-19 07:45:20 -0700

    add task 0

--- 0-binary_tree_node.c
+++ 0-binary_tree_node.c
@@ -4,29 +4,28 @@
  * binary_tree_node - main.
  * @parent: nodo.
  * @value: int
- * 
  * Return: nodo
  **/
 
 binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
 {
-    binary_tree_t *nodo;
+	binary_tree_t *nodo;
 
-    nodo = malloc(sizeof(binary_tree_t));
-    if (nodo == NULL)
-        return (NULL);
+	nodo = malloc(sizeof(binary_tree_t));
+	if (nodo == NULL)
+		return (NULL);
 
-    nodo->n = value;
-    nodo->left = nodo->right = NULL; 
+	nodo->n = value;
+	nodo->left = nodo->right = NULL;
 
-    if (parent == NULL)
-    {
-        nodo->parent = NULL;
-        return (nodo);
-    }
-    else
-    {
-        nodo->parent = parent;
-        return (nodo);
-    }
+	if (parent == NULL)
+	{
+		nodo->parent = NULL;
+		return (nodo);
+	}
+	else
+	{
+		nodo->parent = parent;
+		return (nodo);
+	}
 }
