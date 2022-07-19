16a5545af93251a4ed6314d78a4a7623a213349a
oxyvans <benja.1999.g@hotmail.com>
2022-07-19 08:44:10 -0700

    add task 6, 7 and 8

--- 7-binary_tree_inorder.c
+++ 7-binary_tree_inorder.c
@@ -4,14 +4,13 @@
  * binary_tree_inorder - main.
  * @tree: tree
  * @func: func
- *  
  **/
 
 void binary_tree_inorder(const binary_tree_t *tree, void (*func)(int))
 {
-    if (tree == NULL)
-        return;
-    binary_tree_inorder(tree->left, func);
-    func(tree->n);
-    binary_tree_inorder(tree->right, func);
+	if (tree == NULL && func == NULL)
+		return;
+	binary_tree_inorder(tree->left, func);
+	func(tree->n);
+	binary_tree_inorder(tree->right, func);
 }
