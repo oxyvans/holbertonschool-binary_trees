242b2bb0dad98e9ee8989420206d4b268baa41d8
oxyvans <benja.1999.g@hotmail.com>
2022-07-19 10:59:13 -0700

    add task 9

--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -1,4 +1,5 @@
 #include "binary_trees.h"
+#include "4-binary_tree_is_leaf.c"
 
 /**
  * binary_tree_height - main.
@@ -8,5 +9,17 @@
 
 size_t binary_tree_height(const binary_tree_t *tree)
 {
+	size_t h_left, h_right;
+
+	if (tree == NULL || binary_tree_is_leaf(tree))
+		return (0);
+
+	h_left = binary_tree_height(tree->left);
+	h_right = binary_tree_height(tree->right);
+	if (h_left <= h_right)
+		return (h_right + 1);
+	else
+		return (h_left + 1);
+
 
 }
