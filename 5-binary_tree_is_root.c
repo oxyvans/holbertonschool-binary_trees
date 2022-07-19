d5c16b160eb4b08bd14f004d6f272c586423e09b
oxyvans <benja.1999.g@hotmail.com>
2022-07-19 08:00:18 -0700

    add task 5

--- 5-binary_tree_is_root.c
+++ 5-binary_tree_is_root.c
@@ -4,15 +4,15 @@
  * binary_tree_is_root - main.
  * @node: tree
  *
- * Return: int 
+ * Return: int
  **/
 
 int binary_tree_is_root(const binary_tree_t *node)
 {
-    if (node == NULL)
-        return (0);
-    if (node->parent == NULL)
-        return (1);
-    else
-        return (0);
+	if (node == NULL)
+		return (0);
+	if (node->parent == NULL)
+		return (1);
+	else
+		return (0);
 }
