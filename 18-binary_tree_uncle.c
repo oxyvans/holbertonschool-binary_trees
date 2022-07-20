20b8eeccd749a8ebab7f815774c537f0da26b0d1
oxyvans <benja.1999.g@hotmail.com>
2022-07-20 13:06:37 -0700

    add task 17 and 18

--- 18-binary_tree_uncle.c
+++ 18-binary_tree_uncle.c
@@ -1,4 +1,5 @@
 #include "binary_trees.h"
+#include "17-binary_tree_sibling.c"
 
 /**
  * binary_tree_uncle - main.
@@ -8,5 +9,7 @@
 
 binary_tree_t *binary_tree_uncle(binary_tree_t *node)
 {
-
+	if (node == NULL)
+		return (NULL);
+	return (binary_tree_sibling(node->parent));
 }
