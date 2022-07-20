886900a58c097e59fae7eb42383e526df7b47911
oxyvans <benja.1999.g@hotmail.com>
2022-07-20 09:58:59 -0700

    add task 15

--- 15-binary_tree_is_full.c
+++ 15-binary_tree_is_full.c
@@ -1,4 +1,5 @@
 #include "binary_trees.h"
+#include "4-binary_tree_is_leaf.c"
 
 /**
  * binary_tree_is_full - main.
@@ -8,5 +9,11 @@
 
 int binary_tree_is_full(const binary_tree_t *tree)
 {
-
+	if (tree == NULL)
+		return (0);
+	if (binary_tree_is_leaf(tree))
+		return (1);
+	if (tree->left && tree->right)
+		return (binary_tree_is_full(tree->left) && binary_tree_is_full(tree->right));
+	return (0);
 }
