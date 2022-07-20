c8bb566d1924aa7e01b8ca3ef78a1ceb691b7557
oxyvans <benja.1999.g@hotmail.com>
2022-07-20 08:40:45 -0700

    add task 14 and fix 9

--- 14-binary_tree_balance.c
+++ 14-binary_tree_balance.c
@@ -1,4 +1,5 @@
 #include "binary_trees.h"
+#include "9-binary_tree_height.c"
 
 /**
  * binary_tree_balance - main.
@@ -8,5 +9,8 @@
 
 int binary_tree_balance(const binary_tree_t *tree)
 {
+	if (tree == NULL)
+		return (0);
 
+	return ((int)binary_tree_height(tree->left) - (int)binary_tree_height(tree->right));
 }
--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -1,5 +1,4 @@
 #include "binary_trees.h"
-#include "4-binary_tree_is_leaf.c"
 
 /**
  * binary_tree_height - main.
@@ -11,7 +10,7 @@ size_t binary_tree_height(const binary_tree_t *tree)
 {
 	size_t h_left, h_right;
 
-	if (tree == NULL || binary_tree_is_leaf(tree))
+	if (tree == NULL)
 		return (0);
 
 	h_left = binary_tree_height(tree->left);
