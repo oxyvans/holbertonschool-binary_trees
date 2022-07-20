79f5e7f1171d6e00931b81a825cb277afe29f55b
oxyvans <benja.1999.g@hotmail.com>
2022-07-20 12:57:47 -0700

    add task 16

--- 16-binary_tree_is_perfect.c
+++ 16-binary_tree_is_perfect.c
@@ -1,6 +1,41 @@
 #include "binary_trees.h"
 
 /**
+ * depth - main.
+ * @tree: tree
+ * Return: int
+ **/
+int depth(const binary_tree_t *tree)
+{
+	int d = 0;
+
+	while (tree != NULL)
+	{
+		d++;
+		tree = tree->left;
+	}
+	return (d);
+}
+
+/**
+ * aux - main.
+ * @tree: tree
+ * @d: int
+ * @level : int
+ * Return: nodo
+ **/
+
+int aux(const binary_tree_t *tree, int d, int level)
+{
+	if (tree == NULL)
+		return (1);
+	if (tree->left == NULL && tree->right == NULL)
+		return (d == level + 1);
+	if (tree->left == NULL || tree->right == NULL)
+		return (0);
+	return (aux(tree->left, d, level + 1) && aux(tree->right, d, level + 1));
+}
+/**
  * binary_tree_is_perfect - main.
  * @tree: tree
  * Return: nodo
@@ -8,5 +43,7 @@
 
 int binary_tree_is_perfect(const binary_tree_t *tree)
 {
+	int d = depth(tree);
 
+	return (aux(tree, d, 0));
 }
